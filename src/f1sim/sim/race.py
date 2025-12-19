422ae1f9cf62df6625f283a4e8aa5b8fc4b732d6
James Turner <jturner1825@gmail.com>
2025-12-19 17:42:44 -0500

    Fixed bug with assigning constructors points and win

--- src/f1sim/sim/race.py
+++ src/f1sim/sim/race.py
@@ -3,35 +3,50 @@ import random
 class Race():
     POINTS_TABLE = {1: 25, 2: 18, 3: 15, 4: 12, 5: 10, 6: 8, 7: 6, 8: 4, 9: 2, 10: 1}
 
-    def __init__(self, drivers, teams):
+    def __init__(self, drivers, teams, noise_range=(-10,10)):
         self.drivers = drivers
         self.teams = teams
+        self.noise_range = noise_range
         self.results = []
         self.winner = None
-        self.noise_range = (-10,10)
+        
     
     def simulate_race(self):
+        print("Simulating Race...\n")
+
+        # Result results each race
+        self.results = []
+
+        # Build a quick lookup so we don't nested-loop teams every time
+        team_by_name = {t.name: t for t in self.teams}
+
+
+        # Computer performance = rating + noise
         for driver in self.drivers:
             noise = random.randrange(self.noise_range[0], self.noise_range[1])
             performance = driver.rating + noise
             self.results.append((driver, performance))
         
-        print("Simulating Race...\n")
-        # Sort drivers by random performance
-        self.results = sorted(self.results, key = lambda x: x[1], reverse = True)
+        # Sort drivers by  performance (descending)
+        self.results.sort(key=lambda x: x[1], reverse=True)
         self.winner = self.results[0][0]
 
         # Driver win
         self.winner.add_win()
 
-        # Award driver points
+        # Award driver + constructor points
         for i, (driver, _) in enumerate(self.results, start=1):
             points = self.POINTS_TABLE.get(i,0)
+
             driver.add_points(points)
 
-            # Award team points
-            for team in self.teams:
-                if driver.team == team.name:
-                    team.add_win()
-                    break
+            team = team_by_name.get(driver.team)
+            if team is not None:
+                team.add_points(points)
+
+        # Award constructor win to winner's team
+        winner_team = team_by_name.get(self.winner.team)
+        if winner_team is not None:
+            winner_team.add_win()
+            
         return self.winner, self.results
