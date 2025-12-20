87147f3326af7e1d645cc10db863885a7ee76232
James Turner <jturner1825@gmail.com>
2025-12-19 19:03:36 -0500

    Added rough circuit class implementation

--- src/f1sim/models/circuit.py
+++ src/f1sim/models/circuit.py
@@ -1,3 +1,17 @@
 class Circuit:
-    def __init__(self, name):
+    def __init__(self, name, location, round_number, laps, difficulty):
         self.name = name
+        self.location = location
+        self.round_number = round_number
+        self.laps = laps
+        self.difficulty = difficulty
+        
+    def __str__(self):
+        return(
+            f"Round {self.round_number}\n"
+            f"Name: {self.name}, "
+            f"Location: {self.location}\n"
+            f"{self.laps} Laps\n"
+            f"Difficult: {self.difficulty}\n"
+        )
+        
